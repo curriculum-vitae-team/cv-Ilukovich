c81197299ae5e38bd4d28e443ffaf1be19249425
Denis Iliukovich <dzianis.iliukovich@cogniteq.com>
2023-08-11 12:31:13 +0200

    esling and prettier integration

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    plugins: ['prettier'],
+    rules: {},
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.tsx",
@@ -26,6 +27,9 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "public", "index.html"),
+        }),
+        new ESLintPlugin({
+            extensions: ['js', 'jsx', 'ts', 'tsx'],
         }),
     ],
     devServer: {
