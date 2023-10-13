9643043469dc161529b1d9fc883016c66f042f3c
Denis Iliukovich <dzianis.iliukovich@innowise-group.com>
2023-10-13 12:58:12 +0200

    styles updates

--- src/components/Menu/UserNavigationMenu.tsx
+++ src/components/Menu/UserNavigationMenu.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import Logout from '@mui/icons-material/Logout'
-import Settings from '@mui/icons-material/Settings'
-import { Typography } from '@mui/material'
-import Avatar from '@mui/material/Avatar'
-import Box from '@mui/material/Box'
-import Divider from '@mui/material/Divider'
-import IconButton from '@mui/material/IconButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
-import Tooltip from '@mui/material/Tooltip'
+import { Logout, Settings } from '@mui/icons-material'
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Typography
+} from '@mui/material'
 
 import { logout } from '../../helpers/users/logout'
+
+import './styles.css'
 
 interface UserNavigationMenuProps {
   icon: React.ReactElement
@@ -59,32 +62,6 @@ export const UserNavigationMenu: React.FC<UserNavigationMenuProps> = props => {
         open={open}
         onClose={handleClose}
         onClick={handleClose}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-            mt: 1.5,
-            '& .MuiAvatar-root': {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1
-            },
-            '&:before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0
-            }
-          }
-        }}
         transformOrigin={{ horizontal: 'right', vertical: 'top' }}
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
